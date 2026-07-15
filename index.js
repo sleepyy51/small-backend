4f9311ec646151219293d02e4be55138d101d2b3
Josue Guzman <sleepyy18th@gmail.com>
2026-07-15 01:29:10 -0600

    Add task management endpoints and HTTP request examples

--- index.js
+++ index.js
@@ -28,6 +28,10 @@ app.get('/health', (req, res) => {
     res.json({status : 'ok'});
 });
 
+app.get('/tasks', (req, res) => {
+    res.status(200).json(tasks);
+});
+
 app.get('/tasks/:id', (req, res) => {
     const id = Number(req.params.id);
     const task = tasks.find(task => task.id === id);
@@ -41,10 +45,16 @@ app.get('/tasks/:id', (req, res) => {
 });
 
 app.post('/tasks', (req, res) => {
-    const {title} = req.body;
+    const {title, isDone} = req.body;
     if (!title || title.trim() === "") {
         return res.status(400).json({
             "error": "No valid title found"
+        });
+    }
+
+    if (typeof(isDone) !== "boolean" ) {
+        return res.status(400).json({
+            "error": "No valid task status found"
         });
     }
 
@@ -53,10 +63,52 @@ app.post('/tasks', (req, res) => {
         id = tasks[tasks.length - 1].id + 1;
     }
 
-    tasks.push(new Task(id, title));
+    tasks.push(new Task(id, title, isDone));
     return res.status(201).json({
         "message": "Task created"
     });
+});
+
+app.put('/tasks/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const {title, isDone} = req.body;
+    if (!title || title.trim() === "") {
+        return res.status(400).json({
+            "error": "No valid title found"
+        });
+    }
+
+    if (typeof(isDone) !== "boolean" ) {
+        return res.status(400).json({
+            "error": "No valid task status found"
+        });
+    }
+
+    const task = tasks.find(task => task.id === id);
+    if(!task){
+        return res.status(404).json({
+            "error": `Task ${id} not found`
+        });
+    }
+
+    task.title = title;
+    task.isDone = isDone;
+
+    return res.status(200).json(task);
+});
+
+app.delete('/tasks/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const task = tasks.findIndex(task => task.id === id);
+
+    if(task === -1){
+        return res.status(404).json({
+            "error": `Task ${id} not found`
+        });
+    }
+
+    tasks.splice(task, 1);
+    return res.status(204);
 });
 
 app.listen(port, () => console.log(`Running http://localhost:${port}`))
